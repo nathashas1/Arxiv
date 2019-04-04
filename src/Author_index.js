2bda3e295035de0cef832114be011d27f1f5eb0a
nathashas1 <nathashasur@gmail.com>
2019-04-04 12:20:40 -0700

    fix nil author error

--- src/Author_index.js
+++ src/Author_index.js
@@ -3,14 +3,21 @@ import AuthorIndexItem from './Author_index_item';
 
 const AuthorIndex = ({ entry }) => {
   let authors = Array.from(entry.author)
+  console.log("author error",entry.author)
+  console.log("author error y null",authors,typeof(entry.author))
   const allAuthors = authors.map(author => {
      return (
        <AuthorIndexItem
          authorName={author.name} />
      );
    });
+   const author = <AuthorIndexItem
+     authorName={entry.author.name} />
   return (
-  <div>{allAuthors}</div>
+    <div className='list'>
+        {authors.length ? <div>{allAuthors}</div> : <div>{author}</div>}
+    </div>
+
 );
 };
 
